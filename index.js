14ea0672174963f5ea70c4a41963f242d5f3b2b8
Shubham Sharma <sharmashubham850@gmail.com>
2020-06-18 15:25:52 +0530

    Redesigned autocomplete widget

--- index.js
+++ index.js
@@ -11,24 +11,55 @@ async function fetchData(searchTerm) {
     return response.data.Search;
 }
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search for a movie</b></label>
+    <input type="text" class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultWrapper = document.querySelector('.results');
 
 async function onInput(event) {
     const inputValue = event.target.value;
     const movies = await fetchData(inputValue);
 
-    const targetDiv = document.querySelector('#target');
+    if (movies.length === 0) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
+    dropdown.classList.add('is-active');
+    resultWrapper.innerHTML = '';
+
     for (let movie of movies) {
-        const div = document.createElement('div');
-        div.innerHTML = '';
+        const item = document.createElement('a');
+        item.classList.add('dropdown-item');
 
-        div.innerHTML += `<img src="${movie.Poster}">`;
-        div.innerHTML += `<h1>${movie.Title} (${movie.Year})`;
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-        targetDiv.appendChild(div);
+        item.innerHTML = `
+            <img src="${imgSrc}">
+            ${movie.Title} (${movie.Year})
+        `;
+
+        resultWrapper.appendChild(item);
     }
 
 
 }
 
 input.addEventListener('input', debounce(onInput, delay = 500))
+
+// Dropdown close
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
